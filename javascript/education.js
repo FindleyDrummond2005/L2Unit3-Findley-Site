5ac108c239c4d4f425d1d94400ba48da73a79a5b
findleydrummond2005 <findleydrummondimp@gmail.com>
2023-06-15 18:00:31 +0100

    m

--- javascript/education.js
+++ javascript/education.js
@@ -3,12 +3,10 @@ var education_contents_1 = document.querySelector(".education_contents_1");
 var education_contents_2 = document.querySelector(".education_contents_2");
 var education_contents_3 = document.querySelector(".education_contents_3");
 var education_contents_4 = document.querySelector(".education_contents_4");
-var education_contents_5 = document.querySelector(".education_contents_5");
 var one = true
 var two = true
 var three = true
 var four = true
-var five = true
 
 
 function open_edu(open_edu_num)
@@ -22,14 +20,12 @@ function open_edu(open_edu_num)
             two = true;
             three = true;
             four = true;
-            five = true;
             closed_edu_menu.classList.add("open_edu_menu");
             closed_edu_menu.classList.remove("closed_edu_menu");
             education_contents_1.classList.remove("education_opener")
             education_contents_2.classList.add("education_opener")
             education_contents_3.classList.add("education_opener")
             education_contents_4.classList.add("education_opener")
-            education_contents_5.classList.add("education_opener")
         }
 
         else
@@ -38,7 +34,6 @@ function open_edu(open_edu_num)
             two = true;
             three = true;
             four = true;
-            five = true;
             closed_edu_menu.classList.remove("open_edu_menu");
             closed_edu_menu.classList.add("closed_edu_menu");
             education_contents_1.classList.add("education_opener")
@@ -52,14 +47,12 @@ function open_edu(open_edu_num)
             two = false;
             three = true;
             four = true;
-            five = true;
             closed_edu_menu.classList.add("open_edu_menu");
             closed_edu_menu.classList.remove("closed_edu_menu");
             education_contents_1.classList.add("education_opener")
             education_contents_2.classList.remove("education_opener")
             education_contents_3.classList.add("education_opener")
             education_contents_4.classList.add("education_opener")
-            education_contents_5.classList.add("education_opener")
         }
 
         else
@@ -68,7 +61,6 @@ function open_edu(open_edu_num)
             two = true;
             three = true;
             four = true;
-            five = true;
             closed_edu_menu.classList.remove("open_edu_menu");
             closed_edu_menu.classList.add("closed_edu_menu");
             education_contents_2.classList.add("education_opener")
@@ -82,14 +74,12 @@ function open_edu(open_edu_num)
             two = true;
             three = false;
             four = true;
-            five = true;
             closed_edu_menu.classList.add("open_edu_menu");
             closed_edu_menu.classList.remove("closed_edu_menu");
             education_contents_1.classList.add("education_opener")
             education_contents_2.classList.add("education_opener")
             education_contents_3.classList.remove("education_opener")
             education_contents_4.classList.add("education_opener")
-            education_contents_5.classList.add("education_opener")
         }
 
         else
@@ -98,7 +88,6 @@ function open_edu(open_edu_num)
             two = true;
             three = true;
             four = true;
-            five = true;
             closed_edu_menu.classList.remove("open_edu_menu");
             closed_edu_menu.classList.add("closed_edu_menu");
             education_contents_3.classList.add("education_opener")
@@ -112,14 +101,12 @@ function open_edu(open_edu_num)
             two = true;
             three = true;
             four = false;
-            five = true;
             closed_edu_menu.classList.add("open_edu_menu");
             closed_edu_menu.classList.remove("closed_edu_menu");
             education_contents_1.classList.add("education_opener")
             education_contents_2.classList.add("education_opener")
             education_contents_3.classList.add("education_opener")
             education_contents_4.classList.remove("education_opener")
-            education_contents_5.classList.add("education_opener")
         }
 
         else
@@ -128,41 +115,11 @@ function open_edu(open_edu_num)
             two = true;
             three = true;
             four = true;
-            five = true;
             closed_edu_menu.classList.remove("open_edu_menu");
             closed_edu_menu.classList.add("closed_edu_menu");
             education_contents_4.classList.add("education_opener")
         }
     break;
-    
-    case 5:
-        if (five  == true)
-        {
-            one = true;
-            two = true;
-            three = true;
-            four = true;
-            five = false;
-            closed_edu_menu.classList.add("open_edu_menu");
-            closed_edu_menu.classList.remove("closed_edu_menu");
-            education_contents_1.classList.add("education_opener")
-            education_contents_2.classList.add("education_opener")
-            education_contents_3.classList.add("education_opener")
-            education_contents_4.classList.add("education_opener")
-            education_contents_5.classList.remove("education_opener")
-        }
-    
-        else
-        {
-            one = true;
-            two = true;
-            three = true;
-            four = true;
-            five = true;
-            closed_edu_menu.classList.remove("open_edu_menu");
-            closed_edu_menu.classList.add("closed_edu_menu");
-            education_contents_5.classList.add("education_opener")
-        }
 
     break;
 }
